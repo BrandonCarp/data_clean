a7e8259cb5a7bd2b6982ce7d77c36faab745374d
Brandon <brancarp93@gmail.com>
2025-11-12 12:14:06 -0500

    fastapi update-issues with post request

--- api/main.py
+++ api/main.py
@@ -1,14 +1,21 @@
 from fastapi import FastAPI, UploadFile, File
-from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
 import pandas as pd
+import numpy as np
 from io import StringIO
 from src.cleaning import main as clean_main
+from pydantic import BaseModel
+from typing import Optional, List, Any, Annotated
 
+# Pydantic model for a row of your CSV
+class RowModel(BaseModel):
+    col1: Optional[Any] = None
+    col2: Optional[Any] = None
+    
 
 app = FastAPI()
 
-#  CORS Middleware
+# CORS Middleware
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:3000"],  # front end, replace when live
@@ -17,22 +24,51 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# Routes--!
+# File params w. UploadFile
+
 @app.post("/clean_csv/")
-async def upload_csv(file: UploadFile = File(...)):
-    if file.content_type not in {"text/csv", "application/vnd.ms-excel"}:
-        return JSONResponse(status_code=400, content={"error": "CSV file required"})
+async def upload_csv(file: Annotated[bytes, File()]):
+    
     try:
         contents = await file.read()
         df = pd.read_csv(StringIO(contents.decode("utf-8")))
     except Exception as e:
-        return JSONResponse(status_code=400, content={"error": f"CSV parse failed: {e}"})
+        return {"error": f"CSV parse failed: {e}"}
 
-      
     df_clean = clean_main(df)
-    return JSONResponse(content={"data": df_clean.to_dict(orient="records")})
+   
+    df_clean = df_clean.where(pd.notna(df_clean), None).replace([np.inf, -np.inf], None)
+
+   
+    records = df_clean.to_dict(orient="records")
+
+    return records
 
 
-# @app.get("/csv/") # example get request
-# async def csv_upload():
-#     return {"message": "hey"}
+
+
+
+
+# Routes https://fastapi.tiangolo.com/tutorial/request-files/
+# @app.post("/clean_csv/", response_model=List[dict[str, Any]])
+# async def upload_csv(file: UploadFile = File(...)):
+    
+#     try:
+#         contents = await file.read()
+#         df = pd.read_csv(StringIO(contents.decode("utf-8")))
+#     except Exception as e:
+#         return {"error": f"CSV parse failed: {e}"}
+
+#     df_clean = clean_main(df)
+   
+#     df_clean = df_clean.where(pd.notna(df_clean), None).replace([np.inf, -np.inf], None)
+
+   
+#     records = df_clean.to_dict(orient="records")
+
+#     return records
+
+
+@app.get("/test/") # example get request
+async def test_upload():
+    return {"message": "hey"}
