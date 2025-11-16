17fe2ac4dfa211352106f320147eb71669ae7276
Brandon <brancarp93@gmail.com>
2025-11-15 22:02:35 -0500

    navbar update, preview section inc

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -1,6 +1,26 @@
+import Link from 'next/link'
+import Image from 'next/image'
+import github from '../public/images/github.svg'
+import logo from '../public/images/logo.png'
 
 export default function Navbar(){
   return (
-    <div className="text-2xl ml-5 mt-2">ScrubHub</div>
+    <nav className="flex justify-between items-center mx-5 mt-3">
+      <Image
+        className=''
+      src={logo}
+      width={165}
+      height={165}
+      alt="ScrubHub Logo"
+    />
+ <Link className='text-xl text-black  hover:' href="https://github.com/BrandonCarp/data_clean"><Image
+        className='invert hover:scale-125 '
+      src={github}
+      width={35}
+      height={35}
+      alt="ScrubHub GitHub Link"
+    /></Link>
+        
+    </nav>
   )
 }
---
+++ frontend/components/PreviewSection.tsx
@@ -0,0 +1,8 @@
+
+export default function Preview(){
+  return (
+    <div>
+      <h1>Preview Section</h1>
+    </div>
+  )
+}
