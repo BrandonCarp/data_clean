2324d4f4039365c8fc0f0aa9e885e0b6e77c7d0f
Brandon <69767056+BrandonCarp@users.noreply.github.com>
2025-11-18 16:18:44 -0500

    Update PreviewSection.tsx

--- frontend/components/PreviewSection.tsx
+++ frontend/components/PreviewSection.tsx
@@ -36,48 +36,42 @@ export default function CsvPreview() {
 
   if (loading) return <div>Loading messy CSV…</div>;
 
-
   return (
-  <section className="mt-10 flex flex-col gap-2 px-5">
-    <h2 className="ml-2 text-sm font-semibold text-slate-100">
-      Messy CSV Preview
-    </h2>
+    <section className="mt-10 flex flex-col gap-2 px-5">
+      <h2 className="ml-2 text-sm font-semibold text-slate-100">
+        Messy CSV Preview
+      </h2>
 
-    <div className="overflow-x-auto">
-      <div className="inline-block min-w-full align-middle">
-        {/* outer card */}
-        <div className="overflow-hidden rounded-xl border border-slate-700 bg-slate-900">
-          <table className="min-w-full border-collapse text-xs">
-            <thead>
-              <tr className="bg-slate-900">
+      <div className="overflow-x-auto w-[275px] rounded-xl border border-slate-700 bg-slate-900 text-xs">
+        <table className="overflow-hidden  ">
+          <thead>
+            <tr className="bg-slate-900">
+              {headers.map((h) => (
+                <th
+                  key={h}
+                  className="px-4 py-3 text-left text-slate-100 border-b border-slate-700"
+                >
+                  {h}
+                </th>
+              ))}
+            </tr>
+          </thead>
+          <tbody className="text-slate-200 ">
+            {rows.slice(0, 5).map((row, i) => (
+              <tr
+                key={i}
+                className="bg-slate-800 border-b border-slate-700 last:border-b-0 hover:bg-slate-800/80"
+              >
                 {headers.map((h) => (
-                  <th
-                    key={h}
-                    className="px-4 py-3 text-left text-slate-100 border-b border-slate-700"
-                  >
-                    {h}
-                  </th>
+                  <td key={h} className="px-4 py-3 whitespace-nowrap">
+                    {String(row[h] ?? "")}
+                  </td>
                 ))}
               </tr>
-            </thead>
-            <tbody className="text-slate-200">
-              {rows.slice(0, 5).map((row, i) => (
-                <tr
-                  key={i}
-                  className="bg-slate-800 border-b border-slate-800 last:border-b-0 hover:bg-slate-800/80"
-                >
-                  {headers.map((h) => (
-                    <td key={h} className="px-4 py-3 whitespace-nowrap">
-                      {String(row[h] ?? "")}
-                    </td>
-                  ))}
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
+            ))}
+          </tbody>
+        </table>
       </div>
-    </div>
-  </section>
-);
+    </section>
+  );
 }
