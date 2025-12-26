14f60c6d324247f6b59e4db580028b0fffe14d2d
Brandon <brancarp93@gmail.com>
2025-12-26 16:26:02 -0500

    update

--- frontend/components/PreviewButton.tsx
+++ frontend/components/PreviewButton.tsx
@@ -1,15 +1,26 @@
+import { useState, useEffect } from "react";
+
 type PrevButtonProps = {
   parseCsv: () => Promise<void>;
 };
 
 export default function PreviewButton({ parseCsv }: PrevButtonProps) {
+  const [fileName, setFileName] = useState<string>("Load Messy CSV");
+
+  useEffect(() => {
+    async () => {
+      try {
+      } finally {
+      }
+    };
+  }, []);
   return (
     <div>
       <button
         className="bg-white text-black rounded px-2 cursor-pointer my-2"
         onClick={parseCsv}
       >
-        Preview Button
+        {fileName}
       </button>
     </div>
   );
--- frontend/components/TableSection.tsx
+++ frontend/components/TableSection.tsx
@@ -16,7 +16,7 @@ export default function TableSection() {
   const [headers, setHeaders] = useState<string[]>([]);
 
   async function fetchCsv() {
-    const res = await fetch("/testData/messyTest.csv");
+    const res = await fetch("/data/raw_orders.csv");
     const text = await res.text();
     return text;
   }
@@ -45,7 +45,7 @@ export default function TableSection() {
       <section className="">
         {/* Preview Section */}
         <PreviewButton parseCsv={parseCsv} />
-        {/* {prevTableLoad ? (
+        {prevTableLoad ? (
           <div>Preview Section</div>
         ) : (
           <TableCard
@@ -62,37 +62,7 @@ export default function TableSection() {
               <SparklesIcon className="size-4 mr-1 dark:text-gray-text" />
             }
           />
-        )} */}
-        <div className="space-y-10 mb-5">
-          <TableCard
-            rows={rows}
-            headers={headers}
-            title="Messy CSV"
-            bgColor="bg-amber-100"
-            bdColor="border border-amber-700"
-            textColor="text-amber-800 font-semibold"
-            subtitle="Messy Data with inconsistencies"
-            cleanedRows="Rows to be cleaned"
-            heroicon={<ExclamationCircleIcon className="size-5 mr-1 " />}
-            sparkles={
-              <SparklesIcon className="size-4 mr-1 dark:text-gray-text" />
-            }
-          />
-          <TableCard
-            rows={rows}
-            headers={headers}
-            title="Cleaned CSV"
-            bgColor="bg-emerald-200"
-            bdColor="border border-emerald-500"
-            textColor="text-emerald-900 font-semibold"
-            subtitle="Cleaned Data"
-            cleanedRows="Rows cleaned"
-            heroicon={<CheckCircleIcon className="size-5 mr-1 " />}
-            sparkles={
-              <SparklesIcon className="size-4 mr-1 dark:text-gray-text" />
-            }
-          />
-        </div>
+        )}
       </section>
     </div>
   );
